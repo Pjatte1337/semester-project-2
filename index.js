672ccfe8b968d8391fab4a6f2ac055f4aebf1351
Joakim Vanebo <Pjatte@stud.noroff.no>
2022-12-05 18:28:07 +0100

    change js files

---
+++ index.js
@@ -0,0 +1,13 @@
+import * as f from "./src/js/index.js";
+
+f.signInUser();
+f.signOutBtn();
+f.userSearch();
+f.displayListingUi();
+f.displayProfileInformation();
+f.displayLoggedInButtons();
+f.displaySingleEntryData();
+f.routerSwitch();
+f.signUpUser();
+f.userAvatarUpdate();
+f.addBidListener();
